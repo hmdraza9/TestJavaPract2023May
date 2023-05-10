da67cd681678e2301d2db0ac7398dec8113f1a1b
Hamid Raza <hraza@altimetrik.com>
2023-05-10 15:38:27 +0530

    parent-child-grandchild

---
+++ src/main/java/org/javaPractTwo/TestGrandChild.java
@@ -0,0 +1,40 @@
+package org.javaPractTwo;
+
+public class TestGrandChild extends TestChild{
+
+    TestGrandChild(){
+
+        super();
+
+    }
+
+     String Name = "Nooha";
+     String Email = "Nooha@gmail.com";
+     String phone = "8008222222";
+
+
+    public void showContact(){
+
+        System.out.println("Name: "+Name+"; Email: "+Email+"; phone: "+phone);
+//        System.out.println("Name: "+super.Name+"; Email: "+super.Email+"; phone: "+super.phone);
+
+    }
+    public void showTrigger(){
+
+        showContact();
+        System.out.println("Calling parent");
+        super.showContact();
+
+
+    }
+
+
+    public static void main(String[] args){
+
+        TestGrandChild t1 = new TestGrandChild();
+        t1.showTrigger();
+
+
+    }
+
+}
